b20046fc52d461773dd49b25e9a4a716a9697dd4
lianaipeng <lianaipeng@hichao.com>
2016-01-12 12:32:01 +0800

    add thrift demo

---
+++ thriftDemo/struct/TTG.thrift
@@ -0,0 +1,25 @@
+
+
+
+enum ResponseState{
+	StateOk = 0,
+	StateError = 1,
+	StateEmpty = 2
+}
+
+struct Request{
+	1: i32 studentId = 0
+}
+
+struct Response{
+	1: i32 studentId =0,
+	2: string name,
+	3: list<string> infos,
+	4: ResponseState state
+}
+
+service TTGService{
+	Response getStudentInfo(1:Request request);
+}
+
+
