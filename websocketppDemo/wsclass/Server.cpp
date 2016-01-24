5d22561e61993ed16900d0bf1d2981f2dbea5869
lianaipeng <lianaipeng@hichao.com>
2016-01-24 11:43:21 +0800

    modify websocketpp demo get_con_from_hdl

--- websocketppDemo/wsclass/Server.cpp
+++ websocketppDemo/wsclass/Server.cpp
@@ -6,6 +6,9 @@
 #include <websocketpp/server.hpp>
 
 typedef websocketpp::server<websocketpp::config::asio> server_t;
+//typedef websocketpp::server<debug_custom>::connecttion_ptr connection_ptr;
+typedef websocketpp::http::parser::request   request_t;
+
 
 using websocketpp::connection_hdl;
 using websocketpp::lib::placeholders::_1;
@@ -46,7 +49,11 @@ class WSServer{
     void on_message(connection_hdl hdl, server_t::message_ptr msg) {
         std::cout << "###### on_message()" << std::endl;
         connection_data& data = get_data_from_hdl(hdl);
-
+        //server_t::connection_ptr = m_server.get_con_from_hdl(hdl);
+        server_t::connection_ptr con = m_server.get_con_from_hdl(hdl);
+        request_t req = con->get_request(); 
+        std::cout << "nidaye##### uri:" << req.get_uri()<< ",method:" << req.get_method() << ",body:" << req.get_body() << std::endl; 
+        
         if (data.name == "") {
             data.name = msg->get_payload();
             std::cout << "Setting name of connection with sessionid " << data.sessionid << " to " << data.name << std::endl;
