fb24f3a3ada9afe79163fcdf5b47dc9f90a1bc3f
lianaipeng <lianaipeng@hichao.com>
2016-01-28 10:43:53 +0800

    添加http接口

--- websocketppDemo/wsclass/Server.cpp
+++ websocketppDemo/wsclass/Server.cpp
@@ -8,6 +8,7 @@
 typedef websocketpp::server<websocketpp::config::asio> server_t;
 //typedef websocketpp::server<debug_custom>::connecttion_ptr connection_ptr;
 typedef websocketpp::http::parser::request   request_t;
+typedef websocketpp::http::parser::response  response_t;
 
 
 using websocketpp::connection_hdl;
@@ -28,6 +29,7 @@ class WSServer{
         m_server.set_open_handler(bind(&WSServer::on_open,this,::_1));
         m_server.set_close_handler(bind(&WSServer::on_close,this,::_1));
         m_server.set_message_handler(bind(&WSServer::on_message,this,::_1,::_2));
+        m_server.set_http_handler(bind(&WSServer::on_http,this,::_1));
     }
     void on_open(connection_hdl hdl) {
         std::cout << "###### on_open()" << std::endl;
@@ -49,17 +51,41 @@ class WSServer{
     void on_message(connection_hdl hdl, server_t::message_ptr msg) {
         std::cout << "###### on_message()" << std::endl;
         connection_data& data = get_data_from_hdl(hdl);
-        //server_t::connection_ptr = m_server.get_con_from_hdl(hdl);
+        server_t::connection_ptr con = m_server.get_con_from_hdl(hdl);
+        std::cout << "msg->get_payload():" << msg->get_payload() << std::endl;
+        
+        //con->send("nidaye",msg->get_opcode());
+        m_server.send(hdl,msg->get_payload(),msg->get_opcode());
+        /*
         server_t::connection_ptr con = m_server.get_con_from_hdl(hdl);
         request_t req = con->get_request(); 
         std::cout << "nidaye##### uri:" << req.get_uri()<< ",method:" << req.get_method() << ",body:" << req.get_body() << std::endl; 
-        
+        */
         if (data.name == "") {
             data.name = msg->get_payload();
             std::cout << "Setting name of connection with sessionid " << data.sessionid << " to " << data.name << std::endl;
         } else {
             std::cout << "Got a message from connection " << data.name << " with sessionid " << data.sessionid << std::endl;
         }
+    }
+    void on_http(connection_hdl hdl) {
+        std::cout << "###### on_http()" << std::endl;
+        server_t::connection_ptr con = m_server.get_con_from_hdl(hdl);
+        request_t req = con->get_request(); 
+        const std::string& strUri     = req.get_uri();
+        const std::string& strMethod  = req.get_method();
+        const std::string& strBody    = req.get_body();
+        const std::string& strVersion = req.get_version();
+        std::cout << " uri:" << strUri<< ",method:" << strMethod << ",body:" << strBody << ",version:" << strVersion << std::endl; 
+
+        // 返回curl请求
+        con->set_body("everything is ok now!");
+        con->set_status(websocketpp::http::status_code::value(600));
+        /*
+        response_t rsp;
+        std::string strContent = req.raw();
+        rsp.consume(strContent.c_str(),strContent.size());
+        */
     }
     connection_data& get_data_from_hdl(connection_hdl hdl) {
         con_list::iterator it = m_connections.find(hdl);
